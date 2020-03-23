25215f94f697b21046d3ad45d856fe0cfd9ed6dc
Haoliang-rp <caohaoliang5@gmail.com>
2020-03-23 16:52:21 +0100

    run.py

---
+++ baseline_model/run.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import argparse
+import copy, json, os
+
+import torch
+from torch import nn, optim
+from tensorboardX import SummaryWriter
+from time import gmtime, strftime
+
+from model.model import BiDAF
+from model.data import SQuAD
+from model.ema import EMA
+import evaluate
+
+def train(args, data):
+    device = torch.device(f"cuda:{args.gpu}" if torch.cuda.is_available() else "cpu")
+    model = BiDAF(args, data.WORD.vocab.vectors).to(device)
+    print('end of the test')
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--char-dim', default=8, type=int)
+    parser.add_argument('--char-channel-width', default=5, type=int)
+    parser.add_argument('--char-channel-size', default=100, type=int)
+    parser.add_argument('--context-threshold', default=400, type=int)
+    parser.add_argument('--dev-batch-size', default=100, type=int)
+    parser.add_argument('--dev-file', default='dev-v1.1.json')
+    parser.add_argument('--dropout', default=0.2, type=float)
+    parser.add_argument('--epoch', default=12, type=int)
+    parser.add_argument('--exp-decay-rate', default=0.999, type=float)
+    parser.add_argument('--gpu', default=0, type=int)
+    parser.add_argument('--hidden-size', default=100, type=int)
+    parser.add_argument('--learning-rate', default=0.5, type=float)
+    parser.add_argument('--print-freq', default=250, type=int)
+    parser.add_argument('--train-batch-size', default=60, type=int)
+    parser.add_argument('--train-file', default='train-v1.1.json')
+    parser.add_argument('--word-dim', default=100, type=int)
+    args = parser.parse_args()
+
+    print('loading SQuAD data...')
+    data = SQuAD(args)
+    setattr(args, 'char_vocab_size', len(data.CHAR.vocab))
+    setattr(args, 'word_vocab_size', len(data.WORD.vocab))
+    setattr(args, 'dataset_file', f'.data/squad/{args.dev_file}')
+    setattr(args, 'prediction_file', f'prediction{args.gpu}.out')
+    setattr(args, 'model_time', strftime('%H:%M:%S', gmtime()))
+    print('data loading complete!')
+
+    print('training start!')
+    best_model = train(args, data)
+    if not os.path.exists('saved_models'):
+        os.makedirs('saved_models')
+    torch.save(best_model.state_dict(), f'saved_models/BiDAF_{args.model_time}.pt')
+    print('training finished!')
+
+
+if __name__ == '__main__':
+    main()
