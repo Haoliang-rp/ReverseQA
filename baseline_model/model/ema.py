8ce2705e1ff6996dd8ce4392805fa6c22c091aaf
Haoliang-rp <caohaoliang5@gmail.com>
2020-04-23 10:08:46 +0200

    add ema

---
+++ baseline_model/model/ema.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 10:08:09 2020
+
+@author: HP
+"""
+
+class EMA():
+    def __init__(self, mu):
+        self.mu = mu
+        self.shadow = {}
+
+    def register(self, name, val):
+        self.shadow[name] = val.clone()
+
+    def get(self, name):
+        return self.shadow[name]
+
+    def update(self, name, x):
+        assert name in self.shadow
+        new_average = (1.0 - self.mu) * x + self.mu * self.shadow[name]
+        self.shadow[name] = new_average.clone()
