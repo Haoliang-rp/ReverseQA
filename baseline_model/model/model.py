56c6eab3fe8ac789ac73848c827d9cb8763b3a0e
Haoliang-rp <caohaoliang5@gmail.com>
2020-04-24 10:18:45 +0200

    d

--- baseline_model/model/model.py
+++ baseline_model/model/model.py
@@ -398,7 +398,7 @@ class Baseline(nn.Module):
         self.ca_att = CQAttention(d_model=args.d_model, dropout=args.dropout).to(self.device)
         
         # decoder
-        self.Decoder = Decoder(output_dim=args.output_dim, n_layers=args.DEC_LAYERS, hidden_size=args.hidden_size, d_model=args.d_model, n_head=args.n_head, dropout=args.dropout, max_length=args.max_len_question+2, device=self.device).to(self.device)
+        self.decoder = Decoder(output_dim=args.output_dim, n_layers=args.DEC_LAYERS, hidden_size=args.hidden_size, d_model=args.d_model, n_head=args.n_head, dropout=args.dropout, max_length=args.max_len_question+2, device=self.device).to(self.device)
     
     def make_enc_mask(self, src):
         c_mask = (src != self.args.pad_idx_encoder).unsqueeze(1).unsqueeze(2)
